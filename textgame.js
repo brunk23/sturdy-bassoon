a1b8adb21f5c5c3d2c1ac0518ef255b005d1ec38
Kevin Brunelle <brunellek@gmail.com>
2016-03-03 21:00:01 -0500

    Minor changes to get input working

--- textgame.js
+++ textgame.js
@@ -2,4 +2,14 @@
  * The actual code for the game
  */
 
-$("#title").text("I totally changed this.");
+function processinput() {
+    $("#world").append("<p>" + $("#he-said").val() + "</p>");
+    $("#he-said").val("");
+}
+
+$("#title").text("There's no escape");
+
+$("#world").append("<p>You are standing in the main lobby. To the "+
+		       "east and west you see doors leading to the "+
+		       "rest of the school. To the north there is a "+
+		       "door leading back to the parking lot.</p>");
