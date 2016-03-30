9cd8da35593d685e79248c3402b89478a60b6a00
Kevin Brunelle <brunellek@gmail.com>
2016-03-29 20:57:01 -0400

    Basic program outline, as discussed in group.
    This is the basic program flow discussed and written in pseudo-code
    in group.  I have turned many of the pseudo-code statements into
    functions that will be need to be completed to make the program
    function.
    
    There is also some test code for the loops.  Comments coming now.

--- textgame.js
+++ textgame.js
@@ -3,6 +3,14 @@
  */
 
 /*
+ * These will be variables used in our program
+ */
+var MaxRemoved = 4;
+var LowestBeads = 30;
+var HighestBeads = 50;
+var remaining_beads = 10;
+
+/*
  * Will return a random int between the two arguments, inclusive.
  */
 function randBetween(low, high) {
@@ -21,46 +29,90 @@ function randBetween(low, high) {
     return j;
 }
 
-var Room = function () {
-    this.description = "";
-    this.look = function () {
-        $("#world").append("<p>" + this.description + "</p>" );
-    }
-    this.exits;
-    this.contents;
-};
+/*
+ * We call this to start the game
+ */
+function start_game() {
+    remaining_beads = randBetween(LowestBeads, HighestBeads);
+    create_beads();
+}
 
-var Object = function () {
-    this.description = "";
-    this.look = function () {
-        $("#world").append("<p>" + this.description + "</p>" );
-    }
-    this.drop = function () {
+/*
+ * This will create the beads and put them on the screen.
+ */
+function create_beads() {
 
-    }
-    this.pickup = function () {
+}
 
+/*
+ * Will print the number and update the screen
+ */
+function update_display() {
+
+}
+
+/*
+ * This handles the main game loop
+ */
+function play_game() {
+    while( true ) {
+	update_display();
+	player_turn();
+	if( remaining_beads <= 0 ) {
+	    win_game();
+	    break;
+	}
+	update_display();
+	computer_turn();
+	if( remaining_beads <= 0 ) {
+	    lose_game();
+	    break;
+	}
     }
-};
+}
+
+/*
+ * The code that controls the player's turn
+ */
+function player_turn() {
+    console.log(remaining_beads);
+    remaining_beads--;
+}
+
+/*
+ * The code that controls the computer's turn
+ */
+function computer_turn() {
+    console.log(remaining_beads);
+    remaining_beads -= 2;
+}
+
+/*
+ * We call this if we win
+ */
+function win_game() {
+
+}
+
+/*
+ * We call this if the computer wins
+ */
+function lose_game() {
+
+}
 
 function processinput() {
     $("#world").append("<p>" + $("#he-said").val() + "</p>");
     $("#he-said").val("");
 }
 
-$("#title").text("This is the title");
 
-var lobby = new Room();
-lobby.description = "You are in the lobby of a building.";
+$("#title").text("Take up to " + MaxRemoved);
 
-var cokebottle = new Object();
-cokebottle.description = "It's a coke bottle.";
+/* Start the game */
+function game_loop() {
+    start_game();
+    play_game();
+}
 
-var spritebottle = new Object();
-spritebottle.description = "This sprite bottle is empty.";
-
-
-lobby.look();
-cokebottle.look();
-spritebottle.look();
-lobby.look();
+game_loop();
