30e24891deaee5d7306795ba84a03e0a2b7d81d9
Kevin Brunelle <brunellek@gmail.com>
2018-08-01 19:32:47 -0400

    Finish up the basic idea
    Would like to add a time function

--- products/scripts/products.js
+++ products/scripts/products.js
@@ -14,8 +14,29 @@ function randBetween(low, high) {
  * the type of problem.
  */
 function Problem(difficulty) {
-    this.multiplicand = randBetween(11,99);
-    this.multiplier = randBetween(2,10);
+    if( "easy" == difficulty ) {
+	var tmp = randBetween(1,2);
+	if( tmp == 1 ) {
+	    this.multiplicand = randBetween(1,19);
+	    this.multiplicand *= 5;
+	    this.multiplier = randBetween(2,9);
+	} else {
+	    this.multiplier = randBetween(1,9);
+	    this.multiplicand = randBetween(1,9);
+	    this.multiplicand *= 11;
+	}
+    } else {
+	if( "normal" == difficulty ) {
+	    this.multiplicand = randBetween(11,99);
+	    this.multiplier = randBetween(2,10);
+	} else {
+	    if( "hard" != difficulty ) {
+		console.log("Invalid difficulty level: " + difficulty);
+	    }
+	    this.multiplicand = randBetween(11,99);
+	    this.multiplier = randBetween(2,12);
+	}
+    }
     this.product = this.multiplicand * this.multiplier;
 }
 
@@ -23,19 +44,37 @@ function Problem(difficulty) {
  * Will update the page to show the problem
  */
 function displayProblem() {
+    document.getElementById("top").innerHTML = '<font size="7">'
+	+ prob.multiplicand + '</font>';
+    document.getElementById("bottom").innerHTML = '<font size="7">x '
+	+ prob.multiplier + '</font>';
+    document.getElementById("ans").innerHTML = '<font size="7">?</font>';
+}
 
+/*
+ * Will generate and display a new problem
+ */
+function newProb(level) {
+    prob = new Problem(level);
+    displayProblem();
+    document.getElementById("butt").innerHTML =
+	'<button onclick="displayAnswer()">Answer</button>';
 }
 
 /*
  * Will update the page to show the answer
  */
 function displayAnswer() {
-
+    document.getElementById("ans").innerHTML = '<font size="7">' +
+	prob.product + '</font>';
+    document.getElementById("butt").innerHTML =
+	'<button onclick=newProb("easy")>New Easy Problem</button>' +
+	'<button onclick=newProb("normal")>New Normal Problem</button>' +
+	'<button onclick=newProb("hard")>New Hard Problem</button>';
 }
 
 var prob;
 
 window.onload = function() {
-    prob = new Problem("easy");
-    displayProblem();
+    newProb("normal");
 }
